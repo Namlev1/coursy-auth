f7f98d1f18a29675bf0c8718c05fed94ab188054
namlev <mikipatkowski@protonmail.com>
2025-04-01 18:41:23 +0200

    Repo init

---
+++ src/main/kotlin/com/coursy/masterauthservice/MasterAuthServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.coursy.masterauthservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MasterAuthServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MasterAuthServiceApplication>(*args)
+}
