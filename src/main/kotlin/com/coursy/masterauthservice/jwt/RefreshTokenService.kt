946aec275141bde6b94be50bf3281d6dc14ae49b
namlev <mikipatkowski@protonmail.com>
2025-04-26 16:21:34 +0200

    Bugfix: logging twice causes user id constrain violation

--- src/main/kotlin/com/coursy/masterauthservice/jwt/RefreshTokenService.kt
+++ src/main/kotlin/com/coursy/masterauthservice/jwt/RefreshTokenService.kt
@@ -8,6 +8,7 @@ import com.coursy.masterauthservice.failure.UserFailure
 import com.coursy.masterauthservice.model.RefreshToken
 import com.coursy.masterauthservice.repository.RefreshTokenRepository
 import com.coursy.masterauthservice.repository.UserRepository
+import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.time.Instant
@@ -15,6 +16,7 @@ import java.util.*
 import kotlin.jvm.optionals.getOrNull
 
 @Service
+@Transactional
 class RefreshTokenService(
     private val refreshTokenRepository: RefreshTokenRepository,
     private val userRepository: UserRepository,
@@ -26,6 +28,7 @@ class RefreshTokenService(
             ?: return UserFailure.IdNotExists.left()
 
         refreshTokenRepository.deleteByUser(user)
+        refreshTokenRepository.flush()
 
         val refreshToken = RefreshToken(
             user = user,
