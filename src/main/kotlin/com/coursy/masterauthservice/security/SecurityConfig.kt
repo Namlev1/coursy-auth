5b3b9e511911ee606047aa119e35fb124117b5b5
namlev <mikipatkowski@protonmail.com>
2025-04-26 13:42:13 +0200

    Enable method security in SecurityConfig

--- src/main/kotlin/com/coursy/masterauthservice/security/SecurityConfig.kt
+++ src/main/kotlin/com/coursy/masterauthservice/security/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig(
     private val userDetailsService: UserDetailsServiceImp,
     private val jwtTokenFilter: JwtTokenFilter
@@ -53,7 +55,7 @@ class SecurityConfig(
     @Bean
     fun authenticationProvider(): DaoAuthenticationProvider {
         val authProvider = DaoAuthenticationProvider()
-        authProvider.setUserDetailsService(userDetailsService)
+        authProvider.userDetailsService = userDetailsService
         authProvider.setPasswordEncoder(passwordEncoder())
         return authProvider
     }
