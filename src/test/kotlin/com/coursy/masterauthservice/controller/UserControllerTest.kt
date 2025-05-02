f6c705289242c38d6d970ffdfb89d3e6eadb5121
namlev <mikipatkowski@protonmail.com>
2025-05-02 15:58:10 +0200

    Rewrite to JUnit. Add admin creation test case.

--- src/test/kotlin/com/coursy/masterauthservice/controller/UserControllerTest.kt
+++ src/test/kotlin/com/coursy/masterauthservice/controller/UserControllerTest.kt
@@ -1,35 +1,52 @@
 package com.coursy.masterauthservice.controller
 
+import com.coursy.masterauthservice.dto.JwtResponse
+import com.coursy.masterauthservice.dto.LoginRequest
 import com.coursy.masterauthservice.dto.RegistrationRequest
+import com.coursy.masterauthservice.model.RoleName
 import com.coursy.masterauthservice.repository.UserRepository
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.transaction.annotation.Transactional
+import java.nio.charset.StandardCharsets
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 @ActiveProfiles("test")
-class UserControllerTest(
+class UserControllerTest @Autowired constructor(
     private val mockMvc: MockMvc,
     private val mapper: ObjectMapper,
     private val userRepo: UserRepository,
     private val userController: UserController
-) : DescribeSpec({
-    val url = "/v1/user"
+) {
 
-    describe("User registration") {
-        context("When registering regular user") {
-            context("with correct data") {
-                it("should save the user") {
+    private val userUrl = "/v1/user"
+    private val authUrl = "/v1/auth"
+
+    @Nested
+    inner class `User registration` {
+
+        @Nested
+        inner class `When registering regular user` {
+
+            @Nested
+            inner class `with correct data` {
+
+                @Test
+                fun `should save the user`() {
                     // given
                     val registrationRequest = RegistrationRequest(
                         firstName = "Jan",
@@ -41,23 +58,24 @@ class UserControllerTest(
                     )
 
                     // when
-                    val response = mockMvc.post(url) {
+                    val response = mockMvc.post(userUrl) {
                         contentType = MediaType.APPLICATION_JSON
                         content = mapper.writeValueAsString(registrationRequest)
                     }
-
 
                     // then
                     response.andExpect {
                         status { isCreated() }
                     }
+
                     val savedUser = userRepo.findByEmail(registrationRequest.email)
-                    savedUser shouldNotBe null
-                    savedUser?.firstName?.value shouldBe registrationRequest.firstName
-                    savedUser?.lastName?.value shouldBe registrationRequest.lastName
+                    assertNotNull(savedUser)
+                    assertEquals(registrationRequest.firstName, savedUser?.firstName?.value)
+                    assertEquals(registrationRequest.lastName, savedUser?.lastName?.value)
                 }
 
-                it("should not save 2 users with the same email") {
+                @Test
+                fun `should not save 2 users with the same email`() {
                     // given
                     val registrationRequest = RegistrationRequest(
                         firstName = "Jan",
@@ -69,15 +87,15 @@ class UserControllerTest(
                     )
 
                     // when
-                    val firstResponse = mockMvc.post(url) {
-                        contentType = MediaType.APPLICATION_JSON
-                        content = mapper.writeValueAsString(registrationRequest)
-                    }
-                    val secondResponse = mockMvc.post(url) {
+                    val firstResponse = mockMvc.post(userUrl) {
                         contentType = MediaType.APPLICATION_JSON
                         content = mapper.writeValueAsString(registrationRequest)
                     }
 
+                    val secondResponse = mockMvc.post(userUrl) {
+                        contentType = MediaType.APPLICATION_JSON
+                        content = mapper.writeValueAsString(registrationRequest)
+                    }
 
                     // then
                     firstResponse.andExpect {
@@ -86,17 +104,83 @@ class UserControllerTest(
                     secondResponse.andExpect {
                         status { isConflict() }
                     }
+
                     val users = userRepo.findAll()
-                    users.size shouldBe 1
-                    users[0].email.value shouldBe registrationRequest.email
+                    assertEquals(1, users.size)
+                    assertEquals(registrationRequest.email, users[0].email.value)
                 }
             }
         }
-        context("When registering admin") {
-        }
 
-        context("When registering user with incorrect data") {
+        @Nested
+        inner class `When registering admin` {
 
+            @Nested
+            inner class `with correct data` {
+                @Test
+                fun `should save new admin`() {
+                    // given
+                    val adminSetup = setupAdmin()
+                    val jwt = adminSetup.jwt
+                    val adminRequest = adminSetup.adminRequest
+
+                    val registrationRequest = adminRequest.copy(email = "second_admin@admin.com")
+
+                    // when
+                    val adminResponse = mockMvc.post("$userUrl/admin") {
+                        contentType = MediaType.APPLICATION_JSON
+                        content = mapper.writeValueAsString(registrationRequest)
+                        header("Authorization", "Bearer $jwt")
+                    }
+
+                    // then
+                    adminResponse.andExpect {
+                        status { isCreated() }
+                    }
+
+                    val user = userRepo.findByEmail(registrationRequest.email)
+                    assertNotNull(user)
+                    assertEquals(registrationRequest.email, user?.email?.value)
+                    assertEquals(RoleName.ROLE_ADMIN, user?.role?.name)
+                }
+            }
         }
     }
-})
+
+    private fun setupAdmin(): AdminSetup {
+        val adminRequest = RegistrationRequest(
+            firstName = "AdminName",
+            lastName = "AdminPassword",
+            email = "admin@admin.com",
+            password = "SecurePassword123!",
+            companyName = null,
+            roleName = RoleName.ROLE_ADMIN.toString()
+        )
+        userController.createUser(adminRequest)
+
+        val loginRequest = LoginRequest(
+            email = adminRequest.email,
+            password = adminRequest.password
+        )
+
+        val authResponse = mockMvc.perform(
+            post("$authUrl/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(loginRequest))
+        )
+            .andExpect(status().isOk())
+            .andReturn()
+
+        val jwtResponse = mapper.readValue(
+            authResponse.response.getContentAsString(StandardCharsets.UTF_8),
+            JwtResponse::class.java
+        )
+
+        val jwt = jwtResponse.token
+
+        return AdminSetup(adminRequest, jwt)
+    }
+
+    // Data class to hold admin setup information
+    data class AdminSetup(val adminRequest: RegistrationRequest, val jwt: String)
+}
